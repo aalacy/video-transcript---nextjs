ade4e96c50cf6aec91541848647622af1a305940
Aaron White <128511041+aalacy@users.noreply.github.com>
2023-11-21 08:11:52 -0800

    Frontend (#30)
    * update: increase maxium size for guest users
    
    * feat: textTransform, fontStyle in Design
    
    * style: update
    
    * feat: add vtt file along with video
    
    * fix: disable input after uploaded
    
    * feat: allow transcription on guest mode
    
    * feat: mobile responsive menu for guest user
    
    * fix: style of drawer

--- src/components/common/topbar-home.js
+++ src/components/common/topbar-home.js
@@ -55,87 +55,88 @@ export default function TopbarHome() {
   const [state, setState] = useState(false);
 
   return (
-    <AppBar
-      variant="outlined"
-      position="static"
-      sx={{
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-        boxShadow: "none",
-        bgcolor: "background.paper",
-      }}
-    >
-      <Toolbar
-        variant="dense"
+    <>
+      <AppBar
+        variant="outlined"
+        position="static"
         sx={{
-          py: 1,
-          ml: isNonMobile ? "inherit" : 1,
-          width: 1,
-          backgroundColor: "background.paper",
-          justifyContent: "space-between",
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          boxShadow: "none",
+          bgcolor: "background.paper",
         }}
       >
-        <Logo
-          size={isNonMobile ? "medium" : "small"}
-          width={isNonMobile ? "auto" : "100px"}
-        />
-        {isNonMobile ? (
-          <Box>
-            {menuList.map(({ href, title, style, target, variant }) => (
-              <LinkButton key={title} size="small" variant={variant}>
-                <Link href={href} style={style} target={target}>
-                  {title}
-                </Link>
-              </LinkButton>
-            ))}
-          </Box>
-        ) : (
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={() => setState(!state)}
-            sx={{ mr: 1, display: isNonMobile ? "none" : "inherit" }}
-          >
-            <MenuIcon color="primary" />
-          </IconButton>
-        )}
-        <Drawer
+        <Toolbar
+          variant="dense"
           sx={{
-            display: isNonMobile ? "none" : "inherit",
+            py: 1,
+            ml: isNonMobile ? "inherit" : 1,
             width: 1,
-            height: "200px",
-            zIndex: (theme) => theme.zIndex.drawer,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: 1,
-              boxSizing: "border-box",
-              height: "200px",
-              marginTop: "56px",
-              p: 1,
-              px: 3,
-            },
+            backgroundColor: "background.paper",
+            justifyContent: "space-between",
           }}
-          variant={isNonMobile ? "permanent" : "temporary"}
-          anchor="top"
-          open={state}
-          onClose={() => setState(false)}
         >
-          <List
-            sx={{
-              overflow: "auto",
-            }}
-          >
-            {menuList.map(({ href, title, target }) => (
-              <ListItem key={href} disablePadding>
-                <ListItemButton component={Link} href={href} target={target}>
-                  <ListItemText primary={title} sx={{ textAlign: "right" }} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-      </Toolbar>
-      <Divider />
-    </AppBar>
+          <Logo
+            size={isNonMobile ? "medium" : "small"}
+            width={isNonMobile ? "auto" : "100px"}
+          />
+          {isNonMobile ? (
+            <Box>
+              {menuList.map(({ href, title, style, target, variant }) => (
+                <LinkButton key={title} size="small" variant={variant}>
+                  <Link href={href} style={style} target={target}>
+                    {title}
+                  </Link>
+                </LinkButton>
+              ))}
+            </Box>
+          ) : (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={() => setState(!state)}
+              sx={{ mr: 1, display: isNonMobile ? "none" : "inherit" }}
+            >
+              <MenuIcon color="primary" />
+            </IconButton>
+          )}
+        </Toolbar>
+        <Divider />
+      </AppBar>
+      <Drawer
+        disableScrollLock={true}
+        sx={{
+          display: isNonMobile ? "none" : "inherit",
+          maxHeight: "200px",
+          zIndex: (theme) => theme.zIndex.drawer,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            maxHeight: "200px",
+            marginTop: "56px",
+            p: 1,
+            px: 3,
+          },
+        }}
+        variant={isNonMobile ? "permanent" : "temporary"}
+        anchor="top"
+        open={state}
+        onClose={() => setState(false)}
+      >
+        <List
+          sx={{
+            overflow: "auto",
+          }}
+        >
+          {menuList.map(({ href, title, target }) => (
+            <ListItem key={href} disablePadding>
+              <ListItemButton component={Link} href={href} target={target}>
+                <ListItemText primary={title} sx={{ textAlign: "right" }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
   );
 }
