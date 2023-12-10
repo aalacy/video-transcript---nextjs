7d32d11f137b384a001b2de5b754fd46a77b0f91
aalacy <aaronlacywhite387@gmail.com>
2023-12-10 01:47:07 -0800

    fix: set 10kb

--- src/service/file-service.js
+++ src/service/file-service.js
@@ -6,7 +6,7 @@ export class FileService {
     const { file, lang, width, height } = fileData;
     const key = random();
 
-    const chunkSize = 0.01 * 1024 * 1024; // 5MB (adjust based on your requirements)
+    const chunkSize = 5 * 1024 * 1024; // 5MB (adjust based on your requirements)
     const totalChunks = Math.ceil(file.size / chunkSize);
     const chunkProgress = 10 / totalChunks;
     let chunkNumber = 0;
